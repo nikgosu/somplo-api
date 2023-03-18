b8cb3f84cb1764c97689063b5ede6f06a415cbfb
Nikita Kim <mykyta.k@somplo.com>
2023-03-18 23:47:12 +0200

    created nest js server, connected mongoose

---
+++ src/models/Creative.ts
@@ -0,0 +1,21 @@
+import { Schema, Document } from 'mongoose';
+
+export interface Creative extends Document {
+  readonly animation: string;
+  readonly height: number;
+  readonly horizontalPos: number;
+  readonly imageSize: number;
+  readonly verticalPos: number;
+  readonly width: number;
+  readonly imageSrc: string;
+}
+
+export const CreativeSchema = new Schema({
+  animation: { type: String, required: true },
+  height: { type: Number, required: true },
+  horizontalPos: { type: Number, required: true },
+  imageSize: { type: Number, required: true },
+  verticalPos: { type: Number, required: true },
+  width: { type: Number, required: true },
+  imageSrc: { type: String, required: true },
+}, );
