687c2393f5a680eff1b8dc4d4eb852836b571da9
Nikita Kim <mykyta.k@somplo.com>
2023-03-19 13:32:29 +0200

    created nest js server, connected mongoose

---
+++ src/app.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import {InjectModel} from "@nestjs/mongoose";
+import {Model} from "mongoose";
+import {Creative} from "./models/Creative";
+
+@Injectable()
+export class AppService {
+
+  constructor(@InjectModel('Creative') private readonly creativeModel: Model<Creative>) {
+  }
+  getHello(): string {
+    return 'Hello World!';
+  }
+  async create(user: Creative): Promise<Creative> {
+    const createdUser = new this.creativeModel(user);
+    return createdUser.save();
+  }
+
+  async getAll(): Promise<Creative[]> {
+    return this.creativeModel.find().exec();
+  }
+
+  async getOne(id): Promise<Creative[]> {
+    return this.creativeModel.findOne(id);
+  }
+
+  async update(creative): Promise<Creative[]> {
+    return this.creativeModel.findOneAndUpdate(creative._id, creative);
+  }
+
+  async delete(id): Promise<Creative[]> {
+    return this.creativeModel.findOneAndDelete(id);
+  }
+}
